df5889d149cf297f7a796931ba0475159549dac3
addallno <addallno@outlook.com>
2026-06-05 16:45:15 +0800

    fix: support .mpkg extension in info and extract commands
    - Info.cs: recognize .mpkg files when processing single file input
    - Info.cs: merge .pkg and .mpkg directory enumeration
    - Extract.cs: recognize .mpkg files in single file mode
    - Extract.cs: merge recursive and non-recursive .pkg/.mpkg enumeration

--- RePKG/Command/Extract.cs
+++ RePKG/Command/Extract.cs
@@ -130,7 +130,8 @@ namespace RePKG.Command
 
             if (_options.Recursive)
             {
-                foreach (var file in directoryInfo.EnumerateFiles("*.pkg", SearchOption.AllDirectories))
+                foreach (var file in directoryInfo.EnumerateFiles("*.pkg", SearchOption.AllDirectories)
+                    .Concat(directoryInfo.EnumerateFiles("*.mpkg", SearchOption.AllDirectories)))
                 {
                     if (file.Directory == null || file.Directory.FullName.Length < rootDirectoryLength)
                         ExtractPkg(file);
@@ -143,7 +144,8 @@ namespace RePKG.Command
 
             foreach (var directory in directoryInfo.EnumerateDirectories())
             {
-                foreach (var file in directory.EnumerateFiles("*.pkg"))
+                foreach (var file in directory.EnumerateFiles("*.pkg")
+                    .Concat(directory.EnumerateFiles("*.mpkg")))
                 {
                     ExtractPkg(file, true, directory.FullName.Substring(rootDirectoryLength));
                 }
@@ -154,7 +156,8 @@ namespace RePKG.Command
         {
             Directory.CreateDirectory(_options.OutputDirectory);
 
-            if (fileInfo.Extension.Equals(".pkg", StringComparison.OrdinalIgnoreCase))
+            if (fileInfo.Extension.Equals(".pkg", StringComparison.OrdinalIgnoreCase) || 
+                fileInfo.Extension.Equals(".mpkg", StringComparison.OrdinalIgnoreCase))
                 ExtractPkg(fileInfo);
             else if (fileInfo.Extension.Equals(".tex", StringComparison.OrdinalIgnoreCase))
             {
